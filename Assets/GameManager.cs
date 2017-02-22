a215582f15af761abf81bc1bb7fd1be86fc6a89a
LaBoiteAGogo <gauthier.druel@gmail.com>
2017-02-22 02:28:17 +0100

    rfgr
    rtg

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject objet1;
+    public int mousePressed = 0;
+    public Vector3 mousePosition;
+    public Vector3 diff;
+    public Vector3 test;
+    // Use this for initialization
+    void Start() {
+        test = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (mousePressed == 1) {
+            diff = Input.mousePosition - mousePosition;
+            test.y = diff.y - diff.x;
+            objet1.transform.Rotate(test);
+            mousePosition = Input.mousePosition;
+        }
+    }
+        void OnMouseDown()
+    {
+            mousePressed = 1;
+            mousePosition = Input.mousePosition;
+        }
+
+        void OnMouseUp()
+    {
+            // rotating flag
+            mousePressed = 0;
+        }
+    } 
---
+++ Assets/objet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objet : MonoBehaviour {
+    public Vector3 correct_answer;
+    public Vector3 starting_position;
+    public int level;
+    public int mousePressed = 0;
+    public Vector3 mousePosition;
+    public Vector3 diff;
+    public Vector3 test;
+	public Vector3 test2;
+
+    // Use this for initialization
+    void Start () {
+        this.transform.eulerAngles = starting_position;
+        test = Vector3.zero;
+		test2 = Vector3.zero;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		test2 = this.transform.eulerAngles;
+        if (correct_answer == this.transform.eulerAngles)
+            Debug.Log("bite");
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("cul");
+                 mousePressed = 1;
+                mousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+            mousePressed = 0;
+        if (mousePressed == 1)
+        {
+            diff = Input.mousePosition - mousePosition;
+            test.x = diff.y - diff.x;
+            this.transform.Rotate(test);
+            mousePosition = Input.mousePosition;
+        }
+    }
+
+ 
+    }
+
+
